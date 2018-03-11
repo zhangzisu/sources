ebaa2ed3e26335b91693ae279b988d1d40c7e1a7
ZhangZisu <zhangzisu2003@gmail.com>
2018-03-11 18:53:12 +0800

    UPD

--- 2018-03-11-1/B.cpp
+++ 2018-03-11-1/B.cpp
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <map>
+#include <vector>
 #include <string>
 #define MAXN 500010
 #define BLOCK 6300
@@ -26,7 +26,7 @@ inline void insert(std::string &s, int pos) {
         if (!ch[p][d = s[i] ^ 'a']) ch[p][d] = ++cnt;
         p = ch[p][d];
     }
-    fuck[p] = pos;
+    fuck[pos] = p;
 }
 int first[MAXN], last[MAXN], dfsclk = 0;
 inline void dfs(int x) {
@@ -34,16 +34,17 @@ inline void dfs(int x) {
     if (ch[x][0]) dfs(ch[x][0]), last[x] = std::max(last[x], last[ch[x][0]]);
     if (ch[x][1]) dfs(ch[x][1]), last[x] = std::max(last[x], last[ch[x][1]]);
 }
-int n, m, q, type[MAXN], val[MAXN], w[MAXN], ans[MAXN];
+int n, m, q, type[MAXN], val[MAXN], ddd[MAXN], w[MAXN], ans[MAXN];
 std::string s;
+std::vector<int> his[MAXN];
 int main() {
     std::cin >> n >> m >> q;
     for (int i = 1; i <= n; i++) {
         std::cin >> s;
         insert(s, i);
     }
-    for (int i = 1; i <= n; i++) std::cin >> type[i];
-    for (int i = 1; i <= m; i++) std::cin >> val[i];
+    for (int i = 1; i <= n; i++) std::cin >> type[i], his[i].push_back(i);
+    for (int i = 1; i <= m; i++) std::cin >> val[first[fuck[i]]];
     for (int i = 1; i <= n; i++) std::cin >> w[i];
     dfsclk = 0;
     int x, y;
@@ -63,10 +64,48 @@ int main() {
             }
         } else {
             std::cin >> x >> y;
-            mdf[++ms] = {x, y};
+            mdf[++ms] = {x, (his[x].push_back(y), (int)his[x].size() - 1)};
             ++dfsclk;
         }
     }
     std::sort(qry + 1, qry + qs + 1);
+    int l = 1, r = 0, t = 0;
+    long long res = 0;
+    for(int i = 1;i <= qs;i++){
+        while(t < qry[i].t){
+            ++t; int x = mdf[t].x, y = his[x][mdf[t].y];
+            if(l <= x && x <= r){
+                res -= (w[ddd[type[x]]] - w[ddd[type[x]] - 1]) * val[type[x]];
+                res += (w[ddd[type[y]]] - w[ddd[type[y]] - 1]) * val[type[y]];
+            }
+            ddd[type[x]]--; ddd[type[y]]++; type[x] = y;
+        }
+        while(t > qry[i].t){
+            int x = mdf[t].x, y = his[x][mdf[t].y - 1];
+            if(l <= x && x <= r){
+                res -= (w[ddd[type[x]]] - w[ddd[type[x]] - 1]) * val[type[x]];
+                res += (w[ddd[type[y]]] - w[ddd[type[y]] - 1]) * val[type[y]];
+            }
+            ddd[type[x]]--; ddd[type[y]]++; type[x] = y;
+        }
+        while(l < qry[i].l){
+            res -= (w[ddd[type[l]]] - w[ddd[type[l]] - 1]) * val[type[l]];
+            ddd[l++]--;
+        }
+        while(l > qry[i].l){
+            ddd[--l]++;
+            res += (w[ddd[type[l]]] - w[ddd[type[l]] - 1]) * val[type[l]];
+        }
+        while(r < qry[i].r){
+            ddd[++r]++;
+            res += (w[ddd[type[r]]] - w[ddd[type[r]] - 1]) * val[type[r]];
+        }
+        while(r > qry[i].r){
+            res -= (w[ddd[type[r]]] - w[ddd[type[r]] - 1]) * val[type[r]];
+            ddd[r--]--;
+        }
+        *qry[i].p = res;
+    }
+    for(int i = 1;i <= rq;i++)printf("%d\n", ans[i]);
     return 0;
 }
