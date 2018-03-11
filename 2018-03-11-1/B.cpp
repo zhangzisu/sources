01c9f1164ca741f4914b13d97023f982c83b241e
ZhangZisu <zhangzisu2003@gmail.com>
2018-03-12 06:05:40 +0800

    Finished B

--- 2018-03-11-1/B.cpp
+++ 2018-03-11-1/B.cpp
@@ -2,12 +2,13 @@
 #include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 #define MAXN 500010
 #define BLOCK 6300
 struct Query {
-    int l, r, t, *p;
+    int l, r, t;
+    long long *p;
     inline friend bool operator<(const Query &a, const Query &b) {
         if (a.l / BLOCK == b.l / BLOCK) {
             return a.r == b.r ? a.t < b.t : a.r < b.r;
@@ -19,11 +20,11 @@ struct Modify {
     int x, y;
 } mdf[MAXN];
 int qs, ms, rq;
-int ch[MAXN][2], fuck[MAXN], cnt = 0;
+int ch[MAXN][2], fuck[MAXN], cnt = 1;
 inline void insert(std::string &s, int pos) {
     int p = 1, d;
     for (int i = 0; i < (int)s.length(); i++) {
-        if (!ch[p][d = s[i] ^ 'a']) ch[p][d] = ++cnt;
+        if (!ch[p][d = s[i] - 'a']) ch[p][d] = ++cnt;
         p = ch[p][d];
     }
     fuck[pos] = p;
@@ -34,7 +35,8 @@ inline void dfs(int x) {
     if (ch[x][0]) dfs(ch[x][0]), last[x] = std::max(last[x], last[ch[x][0]]);
     if (ch[x][1]) dfs(ch[x][1]), last[x] = std::max(last[x], last[ch[x][1]]);
 }
-int n, m, q, type[MAXN], val[MAXN], ddd[MAXN], w[MAXN], ans[MAXN];
+int n, m, q, type[MAXN], ddd[MAXN];
+long long ans[MAXN], val[MAXN], w[MAXN];
 std::string s;
 std::vector<int> his[MAXN];
 int main() {
@@ -43,8 +45,12 @@ int main() {
         std::cin >> s;
         insert(s, i);
     }
-    for (int i = 1; i <= n; i++) std::cin >> type[i], his[i].push_back(i);
-    for (int i = 1; i <= m; i++) std::cin >> val[first[fuck[i]]];
+    dfs(1);
+    for (int i = 1; i <= n; i++) {
+        std::cin >> type[first[fuck[i]]];
+        his[i].push_back(type[first[fuck[i]]]);
+    }
+    for (int i = 1; i <= m; i++) std::cin >> val[i];
     for (int i = 1; i <= n; i++) std::cin >> w[i];
     dfsclk = 0;
     int x, y;
@@ -52,13 +58,11 @@ int main() {
         std::cin >> x;
         if (x == 1) {
             std::cin >> s;
-            rq++;
-            qs++;
+            rq++, qs++;
             int p = 1;
-            for (int i = 0; i < (int)s.length(); i++) p = ch[p][s[i] ^ 'a'];
+            for (int i = 0; i < (int)s.length(); i++) p = ch[p][s[i] - 'a'];
             if (!p) {
-                qs--;
-                ans[rq] = 0;
+                qs--, ans[rq] = 0;
             } else {
                 qry[qs] = {first[p], last[p], dfsclk, ans + rq};
             }
@@ -71,41 +75,49 @@ int main() {
     std::sort(qry + 1, qry + qs + 1);
     int l = 1, r = 0, t = 0;
     long long res = 0;
-    for(int i = 1;i <= qs;i++){
-        while(t < qry[i].t){
-            ++t; int x = mdf[t].x, y = his[x][mdf[t].y];
-            if(l <= x && x <= r){
+    for (int i = 1; i <= qs; i++) {
+        while (t < qry[i].t) {
+            ++t;
+            int x = mdf[t].x, y = his[x][mdf[t].y];
+            x = first[fuck[x]];
+            if (l <= x && x <= r) {
                 res -= (w[ddd[type[x]]] - w[ddd[type[x]] - 1]) * val[type[x]];
-                res += (w[ddd[type[y]]] - w[ddd[type[y]] - 1]) * val[type[y]];
+                res += (w[ddd[y] + 1] - w[ddd[y]]) * val[y];
+                ddd[type[x]]--;
+                ddd[y]++;
             }
-            ddd[type[x]]--; ddd[type[y]]++; type[x] = y;
+            type[x] = y;
         }
-        while(t > qry[i].t){
+        while (t > qry[i].t) {
             int x = mdf[t].x, y = his[x][mdf[t].y - 1];
-            if(l <= x && x <= r){
+            x = first[fuck[x]];
+            if (l <= x && x <= r) {
                 res -= (w[ddd[type[x]]] - w[ddd[type[x]] - 1]) * val[type[x]];
-                res += (w[ddd[type[y]]] - w[ddd[type[y]] - 1]) * val[type[y]];
+                res += (w[ddd[y] + 1] - w[ddd[y]]) * val[y];
+                ddd[type[x]]--;
+                ddd[y]++;
             }
-            ddd[type[x]]--; ddd[type[y]]++; type[x] = y;
+            type[x] = y;
+            t--;
         }
-        while(l < qry[i].l){
+        while (l < qry[i].l) {
             res -= (w[ddd[type[l]]] - w[ddd[type[l]] - 1]) * val[type[l]];
-            ddd[l++]--;
+            ddd[type[l++]]--;
         }
-        while(l > qry[i].l){
-            ddd[--l]++;
+        while (l > qry[i].l) {
+            ddd[type[--l]]++;
             res += (w[ddd[type[l]]] - w[ddd[type[l]] - 1]) * val[type[l]];
         }
-        while(r < qry[i].r){
-            ddd[++r]++;
+        while (r < qry[i].r) {
+            ddd[type[++r]]++;
             res += (w[ddd[type[r]]] - w[ddd[type[r]] - 1]) * val[type[r]];
         }
-        while(r > qry[i].r){
+        while (r > qry[i].r) {
             res -= (w[ddd[type[r]]] - w[ddd[type[r]] - 1]) * val[type[r]];
-            ddd[r--]--;
+            ddd[type[r--]]--;
         }
         *qry[i].p = res;
     }
-    for(int i = 1;i <= rq;i++)printf("%d\n", ans[i]);
+    for (int i = 1; i <= rq; i++) std::cout << ans[i] << std::endl;
     return 0;
 }
