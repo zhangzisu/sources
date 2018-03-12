36e550584a2125740fbb0cfed8e7151efb58e198
ZhangZisu <zhangzisu2003@gmail.com>
2018-03-12 11:45:22 +0800

    UPD

--- ZEN/1062.cpp
+++ ZEN/1062.cpp
@@ -2,59 +2,98 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#define MAXN 100010
+#include <string>
+#define MAXN 10
 struct state {
-    int son[26], len, link;
-    inline state() {
+    int len, link, size, son[26];
+    inline state(int len = 0, int size = 0) {
+        this->len = len;
+        this->link = 0;
+        this->size = size;
         memset(son, 0, sizeof(son));
-        len = link = 0;
     }
-    inline int& operator[](int x) {
-        return son[x];
+    inline state(const state& x, int len = 0, int size = 0) {
+        this->len = len;
+        this->link = x.link;
+        this->size = size;
+        memcpy(son, x.son, sizeof(son));
     }
+    inline int& operator[](int index) { return son[index]; }
 } sam[MAXN << 2];
-int cnt = 1, last = 1;
+
+int cnt = 1, pre = 1;
 inline void insert(int x) {
-    int p;
-    if (p = sam[last][x]) {
-        if (sam[p].len == sam[last].len + 1) {
-            last = p;
-        } else {
-            int c = ++cnt;
-            sam[c].len = sam[last].len + 1;
-            sam[c].link = sam[p].link;
-            sam[p].link = c;
-            memcpy(sam[c].son, sam[p].son, sizeof(int) * 26);
-            for (int q = last; q && sam[q][x] == p; q = sam[q].link) sam[q][c] = c;
-            last = c;
-        }
-    } else {
-        int q = last;
-        p = last = ++cnt;
-        sam[p].len = sam[q].len + 1;
-        for (; q && !sam[q][x]; q = sam[q].link) sam[q][c] = p;
-        if (!q) {
-            sam[p].link = 1;
-        } else {
-            int s = sam[q][x];
-            if (sam[s].len == sam[q].len + 1) {
-                sam[p].link = q;
-            } else {
-                int c = ++cnt;
-                sam[c].len = sam[q].len + 1;
-                sam[c].link = sam[s].link;
-                memcpy(sam[c].son, sam[s].son, sizeof(int) * 26);
-                for (; q && sam[q][x] == s; q = sam[q].link) sam[q][x] = c;
-                sam[s].link = sam[p].link = c;
-            }
-        }
-    }
+    int p, q, s, c;
+    sam[p = ++cnt] = state(sam[q = pre].len + 1, 1);
+    pre = p;
+    for (; q && !sam[q][x]; q = sam[q].link) sam[q][x] = p;
+    if (!q) return void(sam[p].link = 1);
+    s = sam[q][x];
+    if (sam[q].len + 1 == sam[s].len) return void(sam[p].link = s);
+    sam[c = ++cnt] = state(sam[s], sam[q].len + 1);
+    for (; q && sam[q][x] == s; q = sam[q].link) sam[q][x] = c;
+    sam[s].link = sam[p].link = c;
 }
+
 int n, m, q, k;
 char buf[MAXN];
+int l[MAXN], r[MAXN];
+int L, R, len[MAXN], pos[MAXN];
+int fa[MAXN][18];
+
+inline void prepare() {
+    for (int i = 1; i <= cnt; i++) fa[i][0] = sam[i].link;
+    for (int d = 1; d < 18; d++)
+        for (int i = 1; i <= cnt; i++) fa[i][d] = fa[fa[i][d - 1]][d - 1];
+}
+
+inline void init(char* buf) {
+    for (int i = 0, p = 1, l = 0; buf[i]; i++) {
+        for (; p && !sam[p][buf[i] - 'a']; p = sam[p][buf[i] - 'a']) l = sam[p].size;
+        if (p) {
+            ++l;
+            p = sam[p][buf[i] - 'a'];
+        } else {
+            p = 1;
+        }
+        len[i + 1] = l;
+        pos[i + 1] = p;
+    }
+}
+
+inline int Query(int p, int l) {
+    for (int d = 17; ~d; d--)
+        if (sam[fa[p][d]].len >= l) p = fa[p][d];
+    return sam[p].size;
+}
+
 int main() {
     scanf("%d%d%d%d", &n, &m, &q, &k);
     scanf("%s", buf);
     for (int i = 0; buf[i]; i++) insert(buf[i] - 'a');
+    for (int i = 1; i <= cnt; i++) printf("LINK[%d]=%d\n", i, sam[i].link);
+    for(int i=1;i<=cnt;i++){
+        for(int j=0;j<26;j++){
+            if(sam[i][j])printf("SON[%d][%c]=%d\n",i,(char)j+'a',sam[i][j]);
+        }
+    }
+
+    for (int i = cnt; i; i--) sam[sam[i].link].size += sam[i].size;
+    puts("DDDDDDDDDDDDDDDDDDDDDDDDDDDDDDDD");
+    for(int i = 1;i <= cnt;i++)printf("NODE[%d] : {len = %d, size = %d}\n", i, sam[i].len, sam[i].size);
+    puts("DONEDONEDONEDONEDONEDONEDONEDONE");
+    sam[1].size = 0;
+    for (int i = 1; i <= m; i++) scanf("%d%d", &l[i], &r[i]),l[i]++,r[i]++;
+    while (q--) {
+        scanf("%s%d%d", buf, &L, &R);
+        init(buf);
+        long long ans = 0;
+        for (int i = L; i <= R; i++) {
+            if (len[r[i]] >= r[i] - l[i] + 1) {
+                ans += Query(pos[r[i]], r[i] - l[i] + 1);
+            }
+        }
+        printf("%lld\n", ans);
+    }
     return 0;
 }
---
+++ tmp.cpp
@@ -0,0 +1,84 @@
+#include <algorithm>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <iostream>
+
+using namespace std;
+#define BLOCK 512
+#define MAXN 200000
+#define ll long long
+#define c(x, y) t[x].c[y]
+#define maxlen(x) t[x].maxlen
+#define link(x) t[x].link
+#define mx(x) t[x].mx
+#define fa(i, j) fa[j][i]
+
+struct Node {
+    int c[26], maxlen, link;
+    ll mx;
+} t[MAXN + 5];
+char s[MAXN + 5];
+int book[MAXN + 5], o[MAXN + 5], L[MAXN + 5], R[MAXN + 5], last, tot, n, m, q, k;
+#define New(p, v) (++tot, maxlen(tot) = p ? maxlen(p) + 1 : 0, mx(tot) = v, tot)
+inline void Append(int x) {
+    int p = last, np = last = New(p, 1), q, nq;
+    for (; p && !c(p, x); c(p, x) = np, p = link(p))
+        ;
+    if (!p)
+        link(np) = 1;
+    else if (maxlen(q = c(p, x)) == maxlen(p) + 1)
+        link(np) = q;
+    else {
+        for (nq = New(p, 0), memcpy(t[nq].c, t[q].c, 104); p && c(p, x) == q; c(p, x) = nq, p = link(p))
+            ;
+        link(nq) = link(q), link(q) = link(np) = nq;
+    }
+}
+
+int LOG[MAXN + 5], fa(MAXN + 5, 20), end[MAXN + 5], len[MAXN + 5];
+ll Ans;
+inline ll FindL(int p, int l) {
+    for (int j = LOG[maxlen(p)]; ~j; maxlen(fa(p, j)) >= l ? p = fa(p, j) : 0, j--)
+        ;
+    return mx(p);
+}
+void Run(int p = 1) {
+    for (int i = 1, l = 0; i <= k; i++){
+        for (; p && !c(p, s[i] - 'a'); p = link(p)) l = maxlen(p);
+        p = p ? ++l, c(p, s[i] - 'a') : 1;
+        len[i] = l, end[i] = p;
+    }
+}
+void Calc() {
+    for (int i = 2; i <= n; LOG[i] = -~LOG[i >> 1], i++)
+        ;
+    for (int i = 1; i <= tot; fa(i, 0) = link(i), i++)
+        ;
+    for (int j = 1, i; j <= LOG[n]; j++)
+        for (i = 1; i <= tot; fa(i, j) = fa(fa(i, j - 1), j - 1), i++)
+            ;
+    for (int i = 1, l, r; i <= q; oi(Ans), oc('\n'), i++) {
+        scanf("%s", s + 1);
+        Run();
+        Ans = 0, l = -~read(), r = -~read();
+        for (; l <= r; len[R[l]] >= R[l] - L[l] + 1 ? Ans += FindL(end[R[l]], R[l] - L[l] + 1) : 0, l++)
+            ;
+    }
+}
+
+int main() {
+    n = read(), m = read(), q = read(), k = read(), tot = 0, last = New(0, 0);
+    scanf("%s", s + 1);
+    for (int i = 1; i <= n; Append(s[i] - 'a'), i++)
+        ;
+    for (int i = tot; i; mx(link(o[i])) += mx(o[i]), i--)
+        ;
+    mx(1) = 0;
+    for (int i = 1; i <= m; L[i] = -~read(), R[i] = -~read(), i++)
+        ;
+    Calc();
+    _END;
+    return 0;
+}
