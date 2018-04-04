df90fa124530d802b37530d260229eb6ecbe7c87
ZhangZisu <zhangzisu@localhost.localdomain>
2018-04-04 21:55:29 +0800

    UPD

--- Codeforces/3545.cpp
+++ Codeforces/3545.cpp
@@ -4,20 +4,17 @@
 #include <cstring>
 #define MAXN 200010
 #define MAXM 500010
-int n, m, q, h[MAXN], g[MAXN], p[MAXN], dat[MAXM], N;
-inline int GetCGC(int x) { return std::lower_bound(dat + 1, dat + N + 1, x) - dat; }
+int n, rn, m, q, h[MAXN], g[MAXN], p[MAXN], dat[MAXM], N;
+inline int GetCGC(int x) { return std::upper_bound(dat + 1, dat + N + 1, x) - dat - 1; }
 inline int find(int x) { return p[x] == x ? x : p[x] = find(p[x]); }
 struct edge {
     int u, v, w;
     inline friend bool operator<(const edge &a, const edge &b) { return a.w < b.w; }
 } e[MAXM];
 int head[MAXN], to[MAXN], next[MAXN], tot = 0;
-inline void $(int u, int v) {
-    next[tot] = head[u], to[tot] = v, head[u] = tot++;
-    printf("%5d->%5d\n", u, v);
-}
+inline void $(int u, int v) { next[tot] = head[u], to[tot] = v, head[u] = tot++; }
 int fsa[MAXN], fsb[MAXN], fa[MAXN][20], dfsclk;
-int root[MAXN], L[MAXN], R[MAXN], sum[MAXN], cnt = 0;
+int root[MAXN], L[MAXN << 2], R[MAXN << 2], sum[MAXN << 2], cnt = 0;
 inline void copy(int x, int y) { L[x] = L[y], R[x] = R[y], sum[x] = sum[y]; }
 void insert(int &n, int N, int l, int r, int v) {
     copy(n = ++cnt, N);
@@ -31,14 +28,14 @@ void insert(int &n, int N, int l, int r, int v) {
 }
 int query(int n0, int n1, int l, int r, int k) {
     if (l == r) return l;
-    int now = sum[L[n0]] - sum[L[n1]], mid = (l + r) >> 1;
-    if (now >= k) return query(L[n0], L[n1], 1, mid, k);
-    return query(R[n0], R[n1], mid + 1, r, k - now);
+    int now = sum[R[n0]] - sum[R[n1]], mid = (l + r) >> 1;
+    if (now >= k) return query(R[n0], R[n1], mid + 1, r, k);
+    return query(L[n0], L[n1], l, mid, k - now);
 }
 void dfs(int x) {
     fsa[x] = ++dfsclk;
     root[dfsclk] = root[dfsclk - 1];
-    if (x <= n) insert(root[dfsclk], root[dfsclk - 1], 1, N, g[x]);
+    if (x <= rn) insert(root[dfsclk], root[dfsclk - 1], 1, rn, h[x]);
     for (int i = head[x]; ~i; i = next[i]) fa[to[i]][0] = x, dfs(to[i]);
     fsb[x] = dfsclk;
 }
@@ -47,11 +44,11 @@ int query(int x, int v, int k) {
         if (g[fa[x][d]] <= v) x = fa[x][d];
     int last = root[fsb[x]], pre = root[fsa[x] - 1];
     if (sum[last] - sum[pre] < k) return 0;
-    return query(last, pre, 1, N, k);
+    return query(last, pre, 1, rn, k);
 }
 int main() {
     freopen("in.txt", "r", stdin);
-    scanf("%d%d%d", &n, &m, &q);
+    scanf("%d%d%d", &n, &m, &q), rn = n;
     memset(head, -1, sizeof(head));
     for (int i = 1; i <= n; i++) scanf("%d", &h[i]);
     for (int i = 1; i <= m; i++) scanf("%d%d%d", &e[i].u, &e[i].v, &e[i].w), dat[++N] = e[i].w;
@@ -70,8 +67,6 @@ int main() {
         $(now, U), $(now, V);
         g[now] = GetCGC(e[i].w);
     }
-    for (int i = 1; i <= n; i++) printf("%5d", g[i]);
-    puts("");
     for (int i = 1; i <= n; i++)
         if (!fsa[i]) dfs(fa[find(i)][0] = find(i));
     for (int d = 1; d < 20; d++)
