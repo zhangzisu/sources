6f00230675be3d9416c33177580f322ab8b569fc
ZhangZisu <zhangzisu@localhost.localdomain>
2018-04-12 20:16:35 +0800

    D!

--- BZOJ/tmp.cpp
+++ BZOJ/tmp.cpp
@@ -1,47 +1,91 @@
 #include <algorithm>
+#include <cctype>
 #include <cstdio>
-#include <cstring>
-#include <vector>
+#define lc (x << 1)
+#define rc (x << 1 | 1)
+#define eps 1e-8
 using namespace std;
-const int N = 2010;
-int n, X;
-vector<int> G[N];
-int dp[N][N], tmp[N], v[N], size[N];
-void dfs(int u, int last) {
-    size[u] = 1;
-    //  for(int i = 0; i <= n; ++i) dp[u][i] = X - v[u];
-    dp[u][0] = X - v[u];
-    for (int i = 0; i < G[u].size(); ++i) {
-        int v = G[u][i];
-        if (v == last) continue;
-        dfs(v, u);
-        memset(tmp, -0x3f3f, sizeof(tmp));
-        for (int j = 0; j <= size[u]; ++j)
-            for (int k = 0; k <= size[v]; ++k) {
-                tmp[j + k + 1] = max(tmp[j + k + 1], dp[u][j] + dp[v][k]);
-                if (dp[v][k] >= 0) tmp[j + k] = max(tmp[j + k], dp[u][j]);
-            }
-        size[u] += size[v];
-        for (int j = 0; j <= size[u] + 1; ++j) dp[u][j] = tmp[j];
+const int N = 500050;
+inline int read() {
+    int x = 0, f = 1;
+    char ch = getchar();
+    while (!isdigit(ch)) {
+        if (ch == '-') f = -1;
+        ch = getchar();
     }
+    while (isdigit(ch)) x = x * 10 + ch - '0', ch = getchar();
+    return x * f;
 }
-int main() {
-    scanf("%d%d", &n, &X);
-    for (int i = 1; i <= n; ++i) scanf("%d", &v[i]);
-    for (int i = 1; i < n; ++i) {
-        int u, v;
-        scanf("%d%d", &u, &v);
-        G[u].push_back(v);
-        G[v].push_back(u);
+struct node {
+    int mn, tag;
+} tree[N << 2];
+inline void update(int x) { tree[x].mn = min(tree[lc].mn, tree[rc].mn); }
+inline void build(int x, int l, int r) {
+    if (l == r) {
+        tree[x].mn = l;
+        return;
     }
-    memset(dp, -0x3f3f, sizeof(dp));
-    dfs(1, 0);
-    for(int i=0;i<=n;i++)printf("%d ", dp[1][i]);
-    puts("");
-    for (int i = 0; i < n; ++i)
-        if (dp[1][i] >= 0) {
-            printf("%d\n", i);
-            return 0;
+    int mid = l + r >> 1;
+    build(lc, l, mid);
+    build(rc, mid + 1, r);
+    update(x);
+}
+inline void pushdown(int x) {
+    if (!tree[x].tag) return;
+    static int tag;
+    tag = tree[x].tag;
+    tree[x].tag = 0;
+    tree[lc].mn += tag;
+    tree[lc].tag += tag;
+    tree[rc].mn += tag;
+    tree[rc].tag += tag;
+}
+inline void modify(int x, int l, int r, int l1, int r1, int v) {
+    if (l1 <= l && r1 >= r) {
+        tree[x].mn += v;
+        tree[x].tag += v;
+        return;
+    }
+    int mid = l + r >> 1;
+    pushdown(x);
+    if (l1 <= mid) modify(lc, l, mid, l1, r1, v);
+    if (r1 > mid) modify(rc, mid + 1, r, l1, r1, v);
+    update(x);
+}
+inline int query(int x, int l, int r, int p) {
+    if (l == r) return tree[x].mn >= p ? l : l + 1;
+    int mid = l + r >> 1;
+    pushdown(x);
+    if (p <= tree[rc].mn)
+        return query(lc, l, mid, p);
+    else
+        return query(rc, mid + 1, r, p);
+}
+inline bool cmp(const int &a, const int &b) { return a > b; }
+int n, d[N], size[N], cnt[N], fa[N], ans[N];
+double k;
+int main() {
+    n = read();
+    scanf("%lf", &k);
+    for (int i = n; i; --i) d[i] = read(), fa[i] = i / k + eps, ++size[i], size[fa[i]] += size[i];
+    sort(d + 1, d + n + 1, cmp);
+    build(1, 1, n);
+    for (int i = n - 1; i; --i)
+        if (d[i] == d[i + 1]) cnt[i] = cnt[i + 1] + 1;
+    for (int i = 1; i <= n; ++i) {
+        if (fa[i] && fa[i - 1] != fa[i]) {
+            modify(1, 1, n, ans[fa[i]], n, size[fa[i]] - 1);
+            printf("MDF %d %d %d\n", ans[fa[i]], n, size[fa[i]] - 1);
         }
+        int x = query(1, 1, n, size[i]);
+        printf("D! %d\n", x);
+        x += cnt[x];
+        ++cnt[x];
+        x -= (cnt[x] - 1);
+        ans[i] = x;
+        modify(1, 1, n, x, n, -size[i]);
+        printf("MDF %d %d %d\n", x, n, -size[i]);
+    }
+    for (int i = 1; i <= n; ++i) printf("%d ", d[ans[i]]);
     return 0;
 }
