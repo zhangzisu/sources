edca11e144d29123dbc935813be373c180bebb7c
Zisu Zhang <zhangzisu2003@gmail.com>
2018-05-21 11:46:26 +0800

    upd

--- 2018-05-20/1.cpp
+++ 2018-05-20/1.cpp
@@ -2,81 +2,34 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <map>
-#include <set>
-#define MAXN 100
+#define MAXN 128
+#define MOD 1000000007
 
-int n, m, a[MAXN], ans = 0;
-std::set<unsigned long long> S0, S1;
-std::map<int, int> ls, mr;
-const unsigned long long base = 23, mod = 998244353;
-
-inline int fpow(int x, int p) {
-    int y = 1;
-    while (p) {
-        if (p & 1) y = 1LL * y * x % mod;
-        x = 1LL * x * x % mod;
-        p >>= 1;
-    }
-    return y;
-}
+int n, m, a[MAXN], f[MAXN][MAXN][MAXN], ans = 0;
 
 int main() {
     scanf("%d", &n);
     m = (n << 1) - 1;
-    bool flag = 1;
-    int ddd0 = 0, ddd1 = 0;
-    for (int i = 1; i <= m; i++) {
-        scanf("%d", &a[i]);
-        flag &= (a[i] <= 2);
-        if (a[i] == 1)
-            ddd0++;
-        else
-            ddd1++;
-    }
-    if (flag) {
-        printf("%d\n", fpow(2, std::min(ddd0, ddd1)));
-        return 0;
-    }
-    std::sort(a + 1, a + m);
-    unsigned long long hs0, hs1;
-
-    do {
-        hs0 = a[1], hs1 = a[1];
-        ls.clear();
-        mr.clear();
-        int mid = a[1], cl = 0, cm = 0;
-
-        for (int i = 1; i < n; i++) {
-            int x = a[i << 1], y = a[i << 1 | 1];
-            (x < mid ? ls : mr)[x]++;
-            (x < mid ? cl : cm)++;
-            (y < mid ? ls : mr)[y]++;
-            (y < mid ? cl : cm)++;
-            if (cl < cm) {
-                ls[mid]++;
-                cl++;
-                mid = mr.begin()->first;
-                if (!--mr.begin()->second) mr.erase(mr.begin()->first);
-                cm--;
+    for (int i = 1; i <= m; i++) scanf("%d", &a[i]);
+    std::sort(a + 1, a + m + 1);
+    auto u = [](int &x, int y) {if((x+=y)>=MOD)x-=MOD; };
+    f[n][a[n] != a[n - 1]][a[n] != a[n + 1]] = 1;
+    for (int i = n; i > 1; i--) {
+        int L = a[i - 1] != a[i - 2];
+        int R = a[m - i + 2] != a[m - i + 3];
+        for (int l = 0; l <= m; l++) {
+            for (int r = 0; r <= m; r++) {
+                int now = f[i][l][r];
+                u(f[i - 1][l + L][r + R], now);
+                for (int k = 0; k < l; k++) u(f[i - 1][k + L][r + R + 1], now);
+                for (int k = 0; k < r; k++) u(f[i - 1][l + L + 1][k + R], now);
             }
-            if (cm < cl) {
-                mr[mid]++;
-                cm++;
-                mid = ls.rbegin()->first;
-                if (!--ls.rbegin()->second) ls.erase(ls.rbegin()->first);
-                cl--;
-            }
-            hs0 = hs0 * base + mid;
-            hs1 = (hs1 * base + mid) % mod;
         }
-        if (S0.count(hs0) && S1.count(hs1)) {
-            continue;
-        }
-        ans++;
-        S0.insert(hs0);
-        S1.insert(hs1);
-    } while (std::next_permutation(a + 1, a + m + 1));
+    }
+    for (int i = 0; i <= m; i++)
+        for (int j = 0; j <= m; j++)
+            u(ans, f[1][i][j]);
+
     printf("%d\n", ans);
     return 0;
 }
--- 2018-05-20/2.cpp
+++ 2018-05-20/2.cpp
@@ -4,36 +4,50 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#define MAXN 60
+#define MAXN 210
 #define EPS 1e-5
 const double PI = acos(-1);
-int n, m, r, _x[MAXN], _y[MAXN], x_[MAXN], y_[MAXN];
+int n, m, k, r, _x[MAXN], _y[MAXN], x_[MAXN], y_[MAXN];
 double ans, rd;
 inline double S(double x) { return x * x * PI; }
 inline double sqr(int x) { return 1. * x * x; }
 inline double dist(int x0, int y0, int x1, int y1) { return sqrt(sqr(x0 - x1) + sqr(y0 - y1)); }
-
+std::pair<double, std::pair<int, int>> dt[MAXN * MAXN];
+int match[MAXN], vis[MAXN], skip[MAXN], ban[MAXN][MAXN];
+bool _(int x) {
+    for (int i = 1; i <= m; i++) {
+        if (ban[x][i]) continue;
+        if (vis[i]) continue;
+        vis[i] = 1;
+        if (!match[i] || _(match[i])) return match[i] = x, 1;
+    }
+    return 0;
+}
 int main() {
     scanf("%d%d%d", &n, &m, &r);
-    assert(n <= 10);
-    assert(m <= 10);
-    for (int i = 0; i < n; i++) scanf("%d", &_x[i]);
-    for (int i = 0; i < n; i++) scanf("%d", &_y[i]);
-    for (int i = 0; i < m; i++) scanf("%d", &x_[i]);
-    for (int i = 0; i < m; i++) scanf("%d", &y_[i]);
-
-    for (int d0 = 0; d0 < (1 << n); d0++) {
-        for (int d1 = 0; d1 < (1 << m); d1++) {
-            if (d0 || d1) {
-                rd = r;
-                for (int i = 0; i < n; i++)
-                    if ((d0 >> i) & 1)
-                        for (int j = 0; j < m; j++)
-                            if ((d1 >> j) & 1)
-                                rd = std::min(rd, dist(_x[i], _y[i], x_[j], y_[j]) / 2);
-                ans = std::max(ans, (__builtin_popcount(d0) + __builtin_popcount(d1)) * S(rd));
-            }
+    for (int i = 1; i <= n; i++) scanf("%d", &_x[i]);
+    for (int i = 1; i <= n; i++) scanf("%d", &_y[i]);
+    for (int i = 1; i <= m; i++) scanf("%d", &x_[i]);
+    for (int i = 1; i <= m; i++) scanf("%d", &y_[i]);
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= m; j++) {
+            double d = dist(_x[i], _y[i], x_[j], y_[j]) / 2;
+            if (d < r + EPS) dt[++k] = {d, {i, j}};
         }
+    }
+    dt[++k] = {r, {0, 0}};
+    std::sort(dt + 1, dt + k + 1);
+    memset(ban, 1, sizeof(ban));
+    int cnt = 0;
+    for (int i = 1; i <= k; i++) {
+        memset(skip, 0, sizeof(skip));
+        for (int i = 1; i <= m; i++) skip[match[i]] = 1;
+        for (int i = 1; i <= n; i++)
+            if (!skip[i] && (memset(vis, 0, sizeof(vis)), _(i))) cnt++;
+        int res = n + m - cnt;
+        // printf("Radius %lf: count %d, ans %lf\n", dt[i].first, res, S(dt[i].first) * res);
+        ans = std::max(ans, S(dt[i].first) * res);
+        ban[dt[i].second.first][dt[i].second.second] = 0;
     }
     printf("%.4lf\n", ans);
     return 0;
