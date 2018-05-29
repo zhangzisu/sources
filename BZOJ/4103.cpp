7cc1765e9ddaddca84cab4ba7e427d567317d99c
ZhangZisu <zhangzisu@outlook.com>
2018-05-29 23:22:58 +0800

    Upd

--- BZOJ/4103.cpp
+++ BZOJ/4103.cpp
@@ -15,9 +15,45 @@ void insert(int &n, int N, int p, int v) {
     insert(trie[n].son[x], trie[N].son[x], p - 1, v);
     trie[n].cnt++;
 }
-
+inline int getNum(int n, int N) {
+    return trie[N].cnt - trie[n].cnt;
+}
+int count(int n, int N, int p, int v, int mask) {
+    if (p < 0) return getNum(n, N);
+    int x = ((v >> p) & 1);
+    int y = ((mask >> p) & 1);
+    if (x) return count(trie[n].son[x ^ y], trie[N].son[x ^ y], p - 1, v, mask);
+    return count(trie[n].son[y], trie[N].son[y], p - 1, v, mask) + getNum(trie[n].son[!y], trie[N].son[!y]);
+}
+inline int get(int x, int y, int l, int r, int mid) {
+    int ret = 0;
+    for (int i = x; i <= y; i++) {
+        ret += count(root[l - 1], root[r], 31, mid, ::x[i]);
+    }
+    return ret;
+}
 int main() {
     scanf("%d%d", &n, &m);
-    root[0] = ++cnt;
+    for (int i = 1; i <= n; i++) scanf("%d", &x[i]);
+    for (int i = 1, y; i <= m; i++) {
+        scanf("%d", &y);
+        insert(root[i], root[i - 1], 31, y);
+    }
+    scanf("%d", &q);
+    while (q--) {
+        int x, y, l, r, k;
+        scanf("%d%d%d%d%d", &x, &y, &l, &r, &k);
+        int L = 0, R = (~0u) >> 2, ans = -1;
+        while (L <= R) {
+            int mid = (0LL + L + R) >> 1;
+            if (get(x, y, l, r, mid) >= k) {
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        printf("%d\n", ans);
+    }
     return 0;
 }
