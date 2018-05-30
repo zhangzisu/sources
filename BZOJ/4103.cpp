ae399c007fb2caf055348041df363b9c76d6546c
ZhangZisu <zhangzisu@outlook.com>
2018-05-30 13:42:52 +0800

    Upd

--- BZOJ/4103.cpp
+++ BZOJ/4103.cpp
@@ -7,30 +7,39 @@
 int n, m, q, x[MAXN], root[MAXM * 35], cnt = 0;
 struct node {
     int cnt, son[2];
-} trie[MAXN * 35];
+} trie[MAXM * 35];
 void insert(int &n, int N, int p, int v) {
-    if (p < 0) return;
     trie[n = ++cnt] = trie[N];
     int x = (v >> p) & 1;
-    insert(trie[n].son[x], trie[N].son[x], p - 1, v);
     trie[n].cnt++;
+    if (p < 0) return;
+    insert(trie[n].son[x], trie[N].son[x], p - 1, v);
 }
 inline int getNum(int n, int N) {
     return trie[N].cnt - trie[n].cnt;
 }
-int count(int n, int N, int p, int v, int mask) {
-    if (p < 0) return getNum(n, N);
-    int x = ((v >> p) & 1);
-    int y = ((mask >> p) & 1);
-    if (x) return count(trie[n].son[x ^ y], trie[N].son[x ^ y], p - 1, v, mask);
-    return count(trie[n].son[y], trie[N].son[y], p - 1, v, mask) + getNum(trie[n].son[!y], trie[N].son[!y]);
-}
-inline int get(int x, int y, int l, int r, int mid) {
-    int ret = 0;
-    for (int i = x; i <= y; i++) {
-        ret += count(root[l - 1], root[r], 31, mid, ::x[i]);
+int posL[MAXN], posR[MAXN];
+int get(int l, int r, int k, int d) {
+    if (d < 0) return 0;
+    int all = 0;
+    for (int i = l; i <= r; i++) {
+        int cur = ((x[i] >> d) & 1) ^ 1;
+        all += getNum(trie[posL[i]].son[cur], trie[posR[i]].son[cur]);
     }
-    return ret;
+    for (int i = l; i <= r; i++) {
+        int cur = ((x[i] >> d) & 1) ^ (all < k) ^ 1;
+        posL[i] = trie[posL[i]].son[cur];
+        posR[i] = trie[posR[i]].son[cur];
+    }
+    return get(l, r, k - (all >= k ? 0 : all), d - 1) | ((all >= k) << d);
+}
+void print(int x, int deep = 0) {
+    if (!x) return;
+    printf("#");
+    for (int i = 0; i < deep; i++) putchar(' ');
+    printf("id: %d, cnt = %d\n", x, trie[x].cnt);
+    print(trie[x].son[0], deep + 2);
+    print(trie[x].son[1], deep + 2);
 }
 int main() {
     scanf("%d%d", &n, &m);
@@ -43,17 +52,8 @@ int main() {
     while (q--) {
         int x, y, l, r, k;
         scanf("%d%d%d%d%d", &x, &y, &l, &r, &k);
-        int L = 0, R = (~0u) >> 2, ans = -1;
-        while (L <= R) {
-            int mid = (0LL + L + R) >> 1;
-            if (get(x, y, l, r, mid) >= k) {
-                ans = mid;
-                R = mid - 1;
-            } else {
-                L = mid + 1;
-            }
-        }
-        printf("%d\n", ans);
+        for (int i = x; i <= y; i++) posL[i] = root[l - 1], posR[i] = root[r];
+        printf("%d\n", get(x, y, k, 31));
     }
     return 0;
 }
