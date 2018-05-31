a7cc9638a01016d8beea6d71d2cbca6d98425431
ZhangZisu <zhangzisu@outlook.com>
2018-05-31 13:57:49 +0800

    Upd

---
+++ HDU/6089.cpp
@@ -0,0 +1,14 @@
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<algorithm>
+#define MAXN 100010
+int t, n, m, k, q;
+int main(){
+    scanf("%d", &t);
+    while(t--){
+        scanf("%d%d%d%d", &n, &m, &k, &q);
+        
+    }
+    return 0;
+}
