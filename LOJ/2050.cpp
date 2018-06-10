0bfd1e32254012655ef29978382c8709fae01268
ZhangZisu <zhangzisu@outlook.com>
2018-06-10 21:35:55 +0800

    upd

--- LOJ/2050.cpp
+++ LOJ/2050.cpp
@@ -3,21 +3,22 @@
 #include <cstdlib>
 #include <cstring>
 #define MAXN 200010
-#define MAXM 1000010
-int n, m, q;
+#define MAXM 5000010
+typedef long long lnt;
+lnt n, m, q;
 namespace tr0 {
-int head[MAXN], to[MAXN << 1], next[MAXN << 1], tot = 0;
-inline void $(int u, int v) {
+lnt head[MAXN], to[MAXN << 1], next[MAXN << 1], tot = 0;
+inline void $(lnt u, lnt v) {
     next[tot] = head[u], to[tot] = v, head[u] = tot++;
     next[tot] = head[v], to[tot] = u, head[v] = tot++;
 }
-int fa[MAXN][18], deep[MAXN], size[MAXN], in[MAXN], out[MAXN], pos[MAXN], dfsclk = 0;
-void dfs(int x) {
-    for (int i = 1; i < 18; i++)
+lnt fa[MAXN][18], deep[MAXN], size[MAXN], in[MAXN], out[MAXN], pos[MAXN], dfsclk = 0;
+void dfs(lnt x) {
+    for (lnt i = 1; i < 18; i++)
         fa[x][i] = fa[fa[x][i - 1]][i - 1];
     size[x] = 1;
     pos[in[x] = ++dfsclk] = x;
-    for (int i = head[x]; ~i; i = next[i]) {
+    for (lnt i = head[x]; ~i; i = next[i]) {
         if (to[i] == fa[x][0]) continue;
         fa[to[i]][0] = x;
         deep[to[i]] = deep[x] + 1;
@@ -26,60 +27,60 @@ void dfs(int x) {
     }
     out[x] = dfsclk;
 }
-inline int lca(int x, int y) {
+inline lnt lca(lnt x, lnt y) {
     if (deep[x] < deep[y]) std::swap(x, y);
-    int delta = deep[x] - deep[y];
-    for (int i = 17; ~i; i--)
+    lnt delta = deep[x] - deep[y];
+    for (lnt i = 17; ~i; i--)
         if ((delta >> i) & 1) x = fa[x][i];
-    for (int i = 17; ~i; i--)
+    for (lnt i = 17; ~i; i--)
         if (fa[x][i] != fa[y][i]) x = fa[x][i], y = fa[y][i];
     return x == y ? x : fa[x][0];
 }
-int root[MAXN], sum[MAXM], L[MAXM], R[MAXM], cnt = 0;
-void copy(int &n, int N) {
+lnt root[MAXN], sum[MAXM], L[MAXM], R[MAXM], cnt = 0;
+void copy(lnt &n, lnt N) {
     n = ++cnt;
     sum[n] = sum[N];
     L[n] = L[N];
     R[n] = R[N];
 }
-void insert(int &n, int N, int l, int r, int p) {
+void insert(lnt &n, lnt N, lnt l, lnt r, lnt p) {
     copy(n, N);
     sum[n]++;
     if (l == r) return;
-    int mid = (l + r) >> 1;
+    lnt mid = (l + r) >> 1;
     if (p <= mid)
         insert(L[n], L[N], l, mid, p);
     else
         insert(R[n], R[N], mid + 1, r, p);
 }
-int query(int n, int N, int l, int r, int p) {
+lnt query(lnt n, lnt N, lnt l, lnt r, lnt p) {
     if (l == r) return l;
-    int mid = (l + r) >> 1;
-    int cL = sum[L[n]] - sum[L[N]];
+    lnt mid = (l + r) >> 1;
+    lnt cL = sum[L[n]] - sum[L[N]];
     if (cL >= p) return query(L[n], L[N], l, mid, p);
-    return query(R[n], R[N], mid + 1, r, p);
+    return query(R[n], R[N], mid + 1, r, p - cL);
 }
 inline void build() {
-    for (int i = 1; i <= n; i++) {
-        // printf("insert => %d\n", pos[i]);
+    for (lnt i = 1; i <= n; i++) {
+        // printf("insert => %lld\n", pos[i]);
         insert(root[i], root[i - 1], 1, n, pos[i]);
     }
 }
 }  // namespace tr0
 namespace tr1 {
-int head[MAXN], to[MAXN << 1], val[MAXN << 1], next[MAXN << 1], tot = 0;
-inline void $(int u, int v, int w) {
-    // printf("ADDE %d %d %d\n", u, v, w);
+lnt head[MAXN], to[MAXN << 1], val[MAXN << 1], next[MAXN << 1], tot = 0;
+inline void $(lnt u, lnt v, lnt w) {
+    // printf("ADDE %lld %lld %lld\n", u, v, w);
     next[tot] = head[u], to[tot] = v, val[tot] = w, head[u] = tot++;
     next[tot] = head[v], to[tot] = u, val[tot] = w, head[v] = tot++;
 }
-int fa[MAXN][18], sum[MAXN][18], deep[MAXN];
-void dfs(int x) {
-    for (int i = 1; i < 18; i++) {
+lnt fa[MAXN][18], sum[MAXN][18], deep[MAXN];
+void dfs(lnt x) {
+    for (lnt i = 1; i < 18; i++) {
         fa[x][i] = fa[fa[x][i - 1]][i - 1];
-        sum[x][i] = sum[sum[x][i - 1]][i - 1] + sum[x][i - 1];
+        sum[x][i] = sum[fa[x][i - 1]][i - 1] + sum[x][i - 1];
     }
-    for (int i = head[x]; ~i; i = next[i]) {
+    for (lnt i = head[x]; ~i; i = next[i]) {
         if (to[i] == fa[x][0]) continue;
         fa[to[i]][0] = x;
         sum[to[i]][0] = val[i];
@@ -87,54 +88,48 @@ void dfs(int x) {
         dfs(to[i]);
     }
 }
-inline int lca(int x, int y) {
+inline lnt lca(lnt x, lnt y) {
     if (deep[x] < deep[y]) std::swap(x, y);
-    int delta = deep[x] - deep[y];
-    for (int i = 17; ~i; i--)
+    lnt delta = deep[x] - deep[y];
+    for (lnt i = 17; ~i; i--)
         if ((delta >> i) & 1) x = fa[x][i];
-    for (int i = 17; ~i; i--)
+    for (lnt i = 17; ~i; i--)
         if (fa[x][i] != fa[y][i]) x = fa[x][i], y = fa[y][i];
     return x == y ? x : fa[x][0];
 }
 }  // namespace tr1
-int sum[MAXN], L[MAXN], R[MAXN];
-int posInOrigin[MAXN], sizePfx[MAXN], from[MAXN], all;
+lnt posInOrigin[MAXN], sizePfx[MAXN], from[MAXN], all;
 struct info {
-    int subTreeID, treeID, rank;
-    inline info(int subTreeID, int treeID, int rank) : subTreeID(subTreeID), treeID(treeID), rank(rank) {}
+    lnt subTreeID, treeID, rank;
+    inline info(lnt subTreeID, lnt treeID, lnt rank) : subTreeID(subTreeID), treeID(treeID), rank(rank) {}
 };
-info getInfo(int to) {
-    int pos = std::lower_bound(sizePfx + 1, sizePfx + all + 1, to) - sizePfx;
-    // printf("POS : %d\n", pos);
-    int ppos = to - sizePfx[pos - 1];
-    // printf("PPOS : %d\n", ppos);
-    // printf("pio : %d\n", posInOrigin[pos]);
-    int pppos = tr0::query(
+info getInfo(lnt to) {
+    lnt pos = std::lower_bound(sizePfx + 1, sizePfx + all + 1, to) - sizePfx;
+    lnt ppos = to - sizePfx[pos - 1];
+    lnt pppos = tr0::query(
         tr0::root[tr0::out[posInOrigin[pos]]],
         tr0::root[tr0::in[posInOrigin[pos]] - 1],
         1,
         n,
         ppos);
-    // printf("PPPOS : %d\n", pppos);
     return info(pos, pppos, ppos);
 }
 int main() {
-    scanf("%d%d%d", &n, &m, &q);
+    scanf("%lld%lld%lld", &n, &m, &q);
     memset(tr0::head, -1, sizeof(tr0::head));
     memset(tr1::head, -1, sizeof(tr1::head));
-    for (int i = 1, u, v; i < n; i++) {
-        scanf("%d%d", &u, &v);
+    for (lnt i = 1, u, v; i < n; i++) {
+        scanf("%lld%lld", &u, &v);
         tr0::$(u, v);
     }
     tr0::dfs(1);
     tr0::build();
     sizePfx[all = 1] = tr0::size[1];
     posInOrigin[1] = 1;
-    for (int i = 1; i <= m; i++) {
-        int x, to;
-        scanf("%d%d", &x, &to);
+    for (lnt i = 1; i <= m; i++) {
+        lnt x, to;
+        scanf("%lld%lld", &x, &to);
         auto info = getInfo(to);
-        // printf("BUILD : {%d, %d, %d}\n", info.subTreeID, info.treeID, info.rank);
         posInOrigin[++all] = x;
         sizePfx[all] = sizePfx[all - 1] + tr0::size[x];
         from[all] = to;
@@ -142,29 +137,54 @@ int main() {
     }
     tr1::dfs(1);
     while (q--) {
-        int l, r;
-        scanf("%d%d", &l, &r);
+        lnt l, r;
+        scanf("%lld%lld", &l, &r);
         auto iL = getInfo(l);
         auto iR = getInfo(r);
-        // printf("L : {%d, %d, %d}\n", iL.subTreeID, iL.treeID, iL.rank);
-        // printf("R : {%d, %d, %d}\n", iR.subTreeID, iR.treeID, iR.rank);
-        int ans = 0;
-        ans += tr0::deep[iL.treeID] - tr0::deep[posInOrigin[iL.subTreeID]];
-        ans += tr0::deep[iR.treeID] - tr0::deep[posInOrigin[iR.subTreeID]];
-        int x = iL.subTreeID, y = iR.subTreeID;
-        int lca = tr1::lca(x, y);
-        ans += tr1::deep[x] + tr1::deep[y] - 2 * tr1::deep[lca];
-        for (int i = 17; ~i; i--)
-            if (tr1::deep[tr1::fa[x][i]] < tr1::deep[lca])
-                x = tr1::fa[x][i], ans += tr1::sum[x][i];
-        for (int i = 17; ~i; i--)
-            if (tr1::deep[tr1::fa[y][i]] < tr1::deep[lca])
-                y = tr1::fa[y][i], ans += tr1::sum[y][i];
-        x = getInfo(from[x]).treeID;
-        y = getInfo(from[y]).treeID;
-        lca = tr0::lca(x, y);
-        ans += tr0::deep[x] + tr0::deep[y] - 2 * tr0::deep[lca];
-        printf("%d\n", ans);
+        lnt x = iL.subTreeID, y = iR.subTreeID;
+        lnt lca = tr1::lca(x, y);
+        lnt ans = 0;
+        if (x == y) {
+            x = iL.treeID;
+            y = iR.treeID;
+            lca = tr0::lca(x, y);
+            ans += tr0::deep[x] + tr0::deep[y] - 2 * tr0::deep[lca];
+        } else if (x == lca) {
+            ans += tr0::deep[iR.treeID] - tr0::deep[posInOrigin[iR.subTreeID]];
+            ans += tr1::deep[y] - tr1::deep[x];
+            for (lnt i = 17; ~i; i--)
+                if (tr1::deep[tr1::fa[y][i]] > tr1::deep[lca])
+                    ans += tr1::sum[y][i], y = tr1::fa[y][i];
+            x = iL.treeID;
+            y = getInfo(from[y]).treeID;
+            lca = tr0::lca(x, y);
+            ans += tr0::deep[x] + tr0::deep[y] - 2 * tr0::deep[lca];
+        } else if (y == lca) {
+            ans += tr0::deep[iL.treeID] - tr0::deep[posInOrigin[iL.subTreeID]];
+            ans += tr1::deep[x] - tr1::deep[y];
+            for (lnt i = 17; ~i; i--)
+                if (tr1::deep[tr1::fa[x][i]] > tr1::deep[lca])
+                    ans += tr1::sum[x][i], x = tr1::fa[x][i];
+            x = getInfo(from[x]).treeID;
+            y = iR.treeID;
+            lca = tr0::lca(x, y);
+            ans += tr0::deep[x] + tr0::deep[y] - 2 * tr0::deep[lca];
+        } else {
+            ans += tr0::deep[iL.treeID] - tr0::deep[posInOrigin[iL.subTreeID]];
+            ans += tr0::deep[iR.treeID] - tr0::deep[posInOrigin[iR.subTreeID]];
+            ans += tr1::deep[x] + tr1::deep[y] - 2 * tr1::deep[lca];
+            for (lnt i = 17; ~i; i--)
+                if (tr1::deep[tr1::fa[x][i]] > tr1::deep[lca])
+                    ans += tr1::sum[x][i], x = tr1::fa[x][i];
+            for (lnt i = 17; ~i; i--)
+                if (tr1::deep[tr1::fa[y][i]] > tr1::deep[lca])
+                    ans += tr1::sum[y][i], y = tr1::fa[y][i];
+            x = getInfo(from[x]).treeID;
+            y = getInfo(from[y]).treeID;
+            lca = tr0::lca(x, y);
+            ans += tr0::deep[x] + tr0::deep[y] - 2 * tr0::deep[lca];
+        }
+        printf("%lld\n", ans);
     }
     return 0;
 }
--- LOJ/lalal.cpp
+++ LOJ/lalal.cpp
@@ -1,73 +1,128 @@
+#include <algorithm>
+#include <cmath>
 #include <cstdio>
-#include <cstdlib>
 #include <cstring>
 #include <iostream>
-#include <queue>
-#define N 100005
 using namespace std;
-int n, m, L, x, y, z, id[N], L2[N], p[N];
-int first[N], ddd[N << 1], frm[N << 1], to[N << 1], val[N << 1], l;
-int q[N], sid[N], st[N], h, dis[N];
-bool fl[N], inQ[N];
-struct T {
-    int l, r;
-};
-bool operator<(T p, T b) { return p.l > b.l; }
-priority_queue<T> S;
-void link(int x, int y, int z) {
-    frm[++l] = x;
-    to[l] = y;
-    val[l] = z;
-    ddd[l] = first[x];
-    first[x] = l;
+typedef long long int64;
+char ch;
+bool ok;
+void read(int &x) {
+    ok = 0;
+    for (ch = getchar(); !isdigit(ch); ch = getchar())
+        if (ch == '-') ok = 1;
+    for (x = 0; isdigit(ch); x = x * 10 + ch - '0', ch = getchar())
+        ;
+    if (ok) x = -x;
 }
-void SPFA(int s, int t, int k) {
-    int head = 0, tail = 1;
-    q[1] = s;
-    while (head != tail) {
-        head %= N;
-        int x = q[++head];
-        inQ[x] = 0;
-        for (int i = first[x]; i; i = ddd[i])
-            if (i != k) {
-                int y = to[i];
-                if (id[y] >= id[t]) {
-                    if (!fl[y])
-                        fl[y] = 1, st[++h] = y, sid[y] = dis[x] + val[i];
-                    else
-                        sid[y] = min(sid[y], dis[x] + val[i]);
-                } else if (dis[y] > dis[x] + val[i]) {
-                    dis[y] = dis[x] + val[i];
-                    if (!inQ[y])
-                        inQ[y] = 1, tail %= N, q[++tail] = y;
-                }
-            }
+void read(int64 &x) {
+    ok = 0;
+    for (ch = getchar(); !isdigit(ch); ch = getchar())
+        if (ch == '-') ok = 1;
+    for (x = 0; isdigit(ch); x = x * 10 + ch - '0', ch = getchar())
+        ;
+    if (ok) x = -x;
+}
+const int maxn = 100000 + 5;
+const int maxm = maxn * 2;
+int n, m, q, idx;
+int64 N, a, b, list[maxn];
+int root[maxn], from[maxn];
+struct Seg {
+    int tot, siz[maxn * 18], son[maxn * 18][2], root[maxn];
+    void insert(int &k, int p, int l, int r, int v) {
+        k = ++tot, siz[k] = siz[p] + 1;
+        if (l == r) return;
+        int m = (l + r) >> 1;
+        if (v <= m)
+            son[k][1] = son[p][1], insert(son[k][0], son[p][0], l, m, v);
+        else
+            son[k][0] = son[p][0], insert(son[k][1], son[p][1], m + 1, r, v);
     }
-    for (int t; h; h--) t = st[h], S.push((T){sid[t] + L2[t], id[t]}), fl[t] = 0;
-}
+    void insert(int idx, int v) { insert(root[idx], root[idx - 1], 1, n, v); }
+    int calc(int x, int y, int k) {
+        x = root[x - 1], y = root[y];
+        int l = 1, r = n, m;
+        while (l < r) {
+            m = (l + r) >> 1;
+            if (siz[son[y][0]] - siz[son[x][0]] >= k)
+                r = m, y = son[y][0], x = son[x][0];
+            else
+                l = m + 1, k -= siz[son[y][0]] - siz[son[x][0]], y = son[y][1], x = son[x][1];
+        }
+        return l;
+    }
+} T;
+int dfn[maxn], last[maxn], siz[maxn];
+struct Graph2 {
+    int tot, now[maxn], son[maxm], pre[maxm], fa[maxn][18], dep[maxn];
+    int64 val[maxm], dis[maxn];
+    void put(int a, int b, int64 c) { pre[++tot] = now[a], now[a] = tot, son[tot] = b, val[tot] = c; }
+    void add(int a, int b, int64 c) { put(a, b, c), put(b, a, c); }
+    void dfs(int u) {
+        for (int i = 0; fa[u][i]; i++) fa[u][i + 1] = fa[fa[u][i]][i];
+        for (int p = now[u], v = son[p]; p; p = pre[p], v = son[p])
+            if (v != fa[u][0])
+                fa[v][0] = u, dep[v] = dep[u] + 1, dis[v] = dis[u] + val[p], dfs(v);
+    }
+    void dfs2(int u) {
+        siz[u] = 1, dfn[u] = ++idx, T.insert(idx, u);
+        for (int p = now[u], v = son[p]; p; p = pre[p], v = son[p])
+            if (v != fa[u][0]) dfs2(v), siz[u] += siz[v];
+        last[u] = idx;
+    }
+    void prepare() { dfs(1), dfs2(1); }
+    void swim(int &u, int h) {
+        for (int i = 17; h; i--)
+            if (h >= (1 << i)) h -= (1 << i), u = fa[u][i];
+    }
+    int get_lca(int u, int v) {
+        if (dep[u] < dep[v]) swap(u, v);
+        swim(u, dep[u] - dep[v]);
+        if (u == v) return u;
+        for (int i = 17; i >= 0; i--)
+            if (fa[u][i] != fa[v][i]) u = fa[u][i], v = fa[v][i];
+        return fa[u][0];
+    }
+    int64 get_dist(int u, int v) { return dis[u] + dis[v] - 2 * dis[get_lca(u, v)]; }
+    int find(int u, int v) {
+        swim(u, dep[u] - dep[v] - 1);
+        return u;
+    }
+} G[2];
+int calc(int64 x) { return lower_bound(list + 1, list + idx + 1, x) - list; }
 int main() {
-    scanf("%d%d%d", &n, &m, &L);
-    for (int i = 1; i <= m; i++)
-        scanf("%d%d%d", &x, &y, &z), link(x, y, z);
-    id[1] = 1;
-    for (int i = 1; i <= L; i++)
-        scanf("%d", &p[i]), id[to[p[i]]] = i + 1;
-    for (int i = L; i >= 1; i--)
-        printf("DDD #1 : %d\n", L2[frm[p[i]]] = L2[to[p[i]]] + val[p[i]]);
-    memset(dis, 0x3f, sizeof dis);
-    dis[1] = 0;
-    for (int i = 1; i <= L; i++) {
-        int u = frm[p[i]], v = to[p[i]];
-        for (SPFA(u, v, p[i]); !S.empty() && S.top().r < id[v]; S.pop())
-            ;
-        puts("DDD #2");
-        for(int i = 1;i <= n;i++)printf("%d ", dis[i]);
-        puts("");
-        puts("DDD #3");
-        for(int i = 1;i <= n;i++)printf("%d ", sid[i]);
-        puts("");
-        printf("%d\n", S.empty() ? -1 : S.top().l);
-        dis[v] = dis[u] + val[p[i]];
+    read(n), read(m), read(q);
+    for (int i = 1; i < n; i++) read(a), read(b), G[0].add(a, b, 1);
+    G[0].prepare(), N = n, idx = 1, root[1] = 1, list[1] = N;
+    for (int i = 1; i <= m; i++) {
+        read(a), read(b);
+        int t = calc(b), r = root[t], bb = T.calc(dfn[r], last[r], b - list[t - 1]);
+        G[1].add(idx + 1, t, G[0].dis[bb] - G[0].dis[r] + 1);
+        N += siz[a], list[++idx] = N, root[idx] = a, from[idx] = bb;
+    }
+    G[1].dfs(1);
+    for (int i = 1; i <= q; i++) {
+        read(a), read(b);
+        int ta = calc(a), ra = root[ta], aa = T.calc(dfn[ra], last[ra], a - list[ta - 1]);
+        int tb = calc(b), rb = root[tb], bb = T.calc(dfn[rb], last[rb], b - list[tb - 1]);
+        printf("{%d, %d}\n", ta, aa);
+        printf("{%d, %d}\n", tb, bb);
+        int lca = G[1].get_lca(ta, tb);
+        int64 res = G[0].dis[aa] - G[0].dis[ra] + G[0].dis[bb] - G[0].dis[rb] + G[1].get_dist(ta, tb);
+        if (ta == tb)
+            printf("%lld\n", G[0].get_dist(aa, bb));
+        else if (ta == lca) {
+            int frb = from[G[1].find(tb, lca)];
+            printf("%lld\n", res - (G[0].dis[aa] + G[0].dis[frb] - G[0].get_dist(aa, frb) - 2 * G[0].dis[ra]));
+        } else if (tb == lca) {
+            int fra = from[G[1].find(ta, lca)];
+            printf("%lld\n", res - (G[0].dis[bb] + G[0].dis[fra] - G[0].get_dist(bb, fra) - 2 * G[0].dis[rb]));
+        } else {
+            int fra = from[G[1].find(ta, lca)];
+            int frb = from[G[1].find(tb, lca)];
+            printf("%lld\n", res - (G[0].dis[fra] + G[0].dis[frb] - G[0].get_dist(fra, frb) - 2 * G[0].dis[root[lca]]));
+        }
     }
     return 0;
 }
