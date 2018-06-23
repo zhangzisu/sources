48b629df73dda80b70c8154b6b0d38d60bf1a357
ZhangZisu <zhangzisu@outlook.com>
2018-06-23 11:37:06 +0800

    upd

--- LOJ/2392.cpp
+++ LOJ/2392.cpp
@@ -6,25 +6,50 @@
 #define MAXN 100010
 typedef long long lnt;
 typedef long double ld;
+const lnt INFLL = (~0ULL) >> 2;
+const int INF = (~0U) >> 2;
 
-lnt n, k, t, x[MAXN];
-inline bool dd(lnt mid) {
-    return (x[k] - x[1] <= mid * t) && (x[n] - x[k - 1] <= mid * t * 2);
-}
-inline bool DD(lnt mid) {
-    return (x[n] - x[k] <= mid * t) && (x[k + 1] - x[1] <= mid * t * 2);
+lnt n, k, t, x[MAXN], y[MAXN];
+inline bool check(lnt mid) {
+    for (int i = 1; i <= n; i++) y[i] = x[i] - 2 * i * mid;
+    int posL = k, posR = k;
+    for (int i = k - 1; i >= 1; i--)
+        if (y[i] >= y[posL]) posL = i;
+    for (int i = k + 1; i <= n; i++)
+        if (y[i] <= y[posR]) posR = i;
+    int l = k, r = k;
+    while (l != posL || r != posR) {
+        int L = l, R = r, flag = 0;
+        while (L > posL && y[L - 1] >= y[r])
+            if (y[--L] >= y[l]) break;
+        if (y[L] >= y[l] && l != L) l = L, flag = 1;
+        while (R < posR && y[R + 1] <= y[l])
+            if (y[++R] <= y[r]) break;
+        if (y[R] <= y[r] && r != R) r = R, flag = 1;
+        if (!flag) break;
+    }
+    if (l != posL || r != posR) return 0;
+    l = 1, r = n;
+    if (y[l] < y[r]) return 0;
+    while (l != posL || r != posR) {
+        int L = l, R = r, flag = 0;
+        while (L < posL && y[L + 1] >= y[r])
+            if (y[++L] >= y[l]) break;
+        if (y[L] >= y[l] && l != L) l = L, flag = 1;
+        while (R > posR && y[R - 1] <= y[l])
+            if (y[--R] <= y[r]) break;
+        if (y[R] <= y[r] && r != R) r = R, flag = 1;
+        if (!flag) break;
+    }
+    return l == posL && r == posR;
 }
 int main() {
     scanf("%lld%lld%lld", &n, &k, &t);
     for (lnt i = 1; i <= n; i++) scanf("%lld", &x[i]);
-    if (k == 1 || k == n) {
-        printf("%lld\n", (lnt)(ceil(.5 * (x[n] - x[1]) / t)));
-        return 0;
-    }
-    lnt l = 0, r = 1000000000, ans = -1;
+    lnt l = 0, r = x[n], ans = 0;
     while (l <= r) {
         lnt mid = (l + r) >> 1;
-        if (dd(mid * 2) || DD(mid * 2))
+        if (check(mid * t))
             ans = mid, r = mid - 1;
         else
             l = mid + 1;
