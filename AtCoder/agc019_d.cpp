acd4ff576deade7c7c6f212df7fe8bc035cff1c6
ZhangZisu <admin@zhangzisu.cn>
2018-08-17 16:00:39 +0800

    upl

--- AtCoder/agc019_d.cpp
+++ AtCoder/agc019_d.cpp
@@ -1,9 +1,19 @@
-#include<cstdio>
-#include<cstdlib>
-#include<cstring>
-#include<algorithm>
-
-int main(){
-    //
-    return 0;
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#define MAXN 2010
+char buf[MAXN];
+int n, a[MAXN], b[MAXN];
+int main() {
+	scanf("%s", buf + 1);
+	n = strlen(buf + 1);
+	for (int i = 1; i <= n; i++) a[0] |= (a[i] = buf[i] == '1');
+	scanf("%s", buf + 1);
+	for (int i = 1; i <= n; i++) b[0] |= (b[i] = buf[i] == '1');
+	if (!b[0]) return puts(a[0] ? "-1" : "0"), 0;
+	for (int d = 0; d < n; d++) {
+		//
+	}
+	return 0;
 }
