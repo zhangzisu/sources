413c17404c43e085f8b5f415e7c425c41988c49f
ZhangZisu <admin@zhangzisu.cn>
2018-08-23 22:28:45 +0800

    upload

--- LOJ/2760.cpp
+++ LOJ/2760.cpp
@@ -4,7 +4,7 @@
 #include <cstring>
 #include <set>
 #include <vector>
-#define MAXN 100010
+#define MAXN 500010
 typedef long long lnt;
 int n, m, q, color;
 struct line_t {
@@ -15,15 +15,19 @@ inline void getIndex(int& x, const std::vector<int>& v) { x = std::lower_bound(v
 std::vector<std::pair<int, int>> hz[MAXN];
 std::vector<int> ad[MAXN], dl[MAXN];
 std::set<int> S;
-int BIT[MAXN];
-inline int lowbit(int x) { return x & -x; }
-inline void add(int x, int v) {
-	for (; x <= m; x += lowbit(x)) BIT[x] += v;
+int sum[MAXN << 2];
+inline void add(int n, int l, int r, int p, int v) {
+	if (l == r) return sum[n] += v, void();
+	int mid = (l + r) >> 1;
+	p <= mid ? add(n << 1, l, mid, p, v) : add(n << 1 | 1, mid + 1, r, p, v);
+	sum[n] = sum[n << 1] + sum[n << 1 | 1];
 }
-inline int qry(int x) {
-	register int v = 0;
-	for (; x; x -= lowbit(x)) v += BIT[x];
-	return v;
+inline int query(int n, int l, int r, int L, int R) {
+	if (l == L && r == R) return sum[n];
+	int mid = (l + r) >> 1;
+	if (R <= mid) return query(n << 1, l, mid, L, R);
+	if (L > mid) return query(n << 1 | 1, mid + 1, r, L, R);
+	return query(n << 1, l, mid, L, mid) + query(n << 1 | 1, mid + 1, r, mid + 1, R);
 }
 int tag[MAXN << 2];
 void pushDown(int n) {
@@ -47,7 +51,7 @@ int& dead(int n, int l, int r, int p) {
 	pushDown(n);
 	return p <= mid ? dead(n << 1, l, mid, p) : dead(n << 1 | 1, mid + 1, r, p);
 }
-int p[MAXN << 2], id[MAXN];
+int p[MAXN << 2], id[MAXN << 2];
 inline int find(int x) { return x == p[x] ? x : p[x] = find(p[x]); }
 inline int merge(int x, int y) {
 	int X = find(x), Y = find(y);
@@ -56,7 +60,7 @@ inline int merge(int x, int y) {
 	return 1;
 }
 inline void declare(int x) {
-	int& val = dead(1, 0, m - 2, x);
+	int& val = dead(1, 0, m, x);
 	if (val) {
 		val = 0;
 		++color;
@@ -64,30 +68,10 @@ inline void declare(int x) {
 	}
 	// fprintf(stderr, "! DECLARE %d -> %d\n", x, id[x]);
 }
-int ans = 0;
+lnt ans = 0;
 int main() {
 	scanf("%d%d%d", &n, &m, &q);
-	for (int i = 1; i <= q; i++) {
-		scanf("%d%d%d%d", &lines[i].x0, &lines[i].y0, &lines[i].x1, &lines[i].y1);
-		if (lines[i].x0 > lines[i].x1) std::swap(lines[i].x0, lines[i].x1);
-		if (lines[i].y0 > lines[i].y1) std::swap(lines[i].y0, lines[i].y1);
-		vx.push_back(lines[i].x0);
-		vx.push_back(lines[i].x1);
-		vy.push_back(lines[i].y0);
-		vy.push_back(lines[i].y1);
-	}
-	vx.push_back(0);
-	vx.push_back(-1);
-	vx.push_back(n);
-	vx.push_back(n + 1);
-	vy.push_back(0);
-	vy.push_back(-1);
-	vy.push_back(m);
-	vy.push_back(m + 1);
-	std::sort(vx.begin(), vx.end());
-	vx.erase(std::unique(vx.begin(), vx.end()), vx.end());
-	std::sort(vy.begin(), vy.end());
-	vy.erase(std::unique(vy.begin(), vy.end()), vy.end());
+	for (int i = 1; i <= q; i++) scanf("%d%d%d%d", &lines[i].x0, &lines[i].y0, &lines[i].x1, &lines[i].y1);
 	++q;
 	lines[q].x0 = lines[q].x1 = 0;
 	lines[q].y0 = 0;
@@ -104,8 +88,22 @@ int main() {
 	lines[q].y0 = lines[q].y1 = m;
 	lines[q].x0 = 0;
 	lines[q].x1 = n;
+	for (int i = 1; i <= q; i++) {
+		if (lines[i].x0 > lines[i].x1) std::swap(lines[i].x0, lines[i].x1);
+		if (lines[i].y0 > lines[i].y1) std::swap(lines[i].y0, lines[i].y1);
+		vx.push_back(lines[i].x0);
+		vx.push_back(lines[i].x1);
+		vy.push_back(lines[i].y0);
+		vy.push_back(lines[i].y1);
+	}
+	vy.push_back(-1);
+	std::sort(vx.begin(), vx.end());
+	vx.erase(std::unique(vx.begin(), vx.end()), vx.end());
+	std::sort(vy.begin(), vy.end());
+	vy.erase(std::unique(vy.begin(), vy.end()), vy.end());
 	n = (int)vx.size();
 	m = (int)vy.size();
+	S.insert(0);
 	for (int i = 1; i <= q; i++) {
 		getIndex(lines[i].x0, vx);
 		getIndex(lines[i].x1, vx);
@@ -126,7 +124,8 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		// fprintf(stderr, "# PROCESS %d\n", i);
 		for (auto v : ad[i]) {
-			add(v, 1);
+			// fprintf(stderr, "> ADD %d: %d\n", v, 1);
+			add(1, 0, m, v, 1);
 			S.insert(v);
 			int left = *--S.lower_bound(v);
 			declare(left);
@@ -135,26 +134,26 @@ int main() {
 		}
 		for (auto h : hz[i]) {
 			int l = h.first, r = h.second;
-			int L = *S.lower_bound(l);
-			int R = *--S.upper_bound(r);
-			if (L < R) {
-				int count = qry(R - 1) - qry(L);
-				ans += count + 1;
-				fprintf(stderr, "$ ans = %d\n", ans);
-				// fprintf(stderr, "RESET %d - %d\n", L, R - 1);
-				reset(1, 0, m - 2, L, R - 1);
+			int count = query(1, 0, m, l, r);
+			if (count >= 2) {
+				int L = *S.lower_bound(l);
+				int R = *--S.upper_bound(r);
+				ans += count - 1;
+				reset(1, 0, m, L, R - 1);
 			}
 		}
 		for (auto v : dl[i]) {
+			// fprintf(stderr, "> ADD %d: %d\n", v, -1);
 			int left = *--S.lower_bound(v);
 			declare(left);
 			declare(v);
 			ans -= merge(id[left], id[v]);
-			fprintf(stderr, "$ ans = %d\n", ans);
-			add(v, -1);
+			// fprintf(stderr, "$ ans = %d\n", ans);
+			add(1, 0, m, v, -1);
 			S.erase(v);
 		}
 	}
-	printf("%d\n", ans);
+	printf("%lld\n", ans);
+	// fprintf(stderr, "%d %lu %lu\n", color, vx.size(), vy.size());
 	return 0;
 }
