14bfdc9f851d3e56ed0107e1bc45bf0b00d875d5
ZhangZisu <admin@zhangzisu.cn>
2018-09-01 21:01:26 +0800

    upload

--- fetcher.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios');
-
-(async () => {
-    const result = await axios.get('https://tieba.baidu.com/p/3357872724')
-    console.log(result);
-})().catch(e => console.log(e)).then(() => process.exit(0));
