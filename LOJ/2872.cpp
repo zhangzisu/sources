b630aca52c0fa34439ebabdda7fa64ec44ceffa2
ZhangZisu <admin@zhangzisu.cn>
2018-10-22 13:19:15 +0800

    upload

---
+++ LOJ/2872.cpp
@@ -0,0 +1,16 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <vector>
+#define MAXN 100010
+#define MAXM 300010
+int n, m;
+int main() {
+	scanf("%d%d", &n, &m);
+	for (int i = 1, a, b, x, y; i <= n; i++) {
+		scanf("%d%d%d%d", &a, &b, &x, &y);
+		//
+	}
+	return 0;
+}
