342968e77e6ef79316ef0d07e983d0bbb78bc4fb
ZhangZisu <admin@zhangzisu.cn>
2018-10-28 13:02:00 +0800

    upd

--- temp/64.cpp
+++ temp/64.cpp
@@ -5,22 +5,25 @@
 #define MAXN 100010
 typedef long long lnt;
 lnt n, m, l, r, a[MAXN], b[MAXN], c[MAXN];
+inline lnt count(lnt val) {
+	lnt cnt = 0;
+	for (int i = 1; i <= n; i++) {
+		cnt += (std::upper_bound(b + 1, b + n + 1, val - a[i]) - b) - 1;
+	}
+	return cnt;
+}
 inline lnt getValue(int k) {
 	lnt l = 0, r = 2000000000LL, ans = -1;
 	while (l <= r) {
 		lnt mid = (l + r) >> 1;
-		lnt cnt = 0;
-		for (int i = 1; i <= n; i++) {
-			cnt += (std::upper_bound(b + 1, b + n + 1, mid - a[i]) - b) - 1;
-		}
-		if (cnt >= k) {
+		if (count(mid) >= k) {
 			ans = mid;
 			r = mid - 1;
 		} else {
 			l = mid + 1;
 		}
 	}
-    return ans;
+	return ans;
 }
 int main() {
 	scanf("%lld%lld%lld", &n, &l, &r);
@@ -31,13 +34,14 @@ int main() {
 	lnt L = getValue(l);
 	lnt R = getValue(r);
 	for (int i = 1; i <= n; i++) {
-		int s = std::lower_bound(b + 1, b + n + 1, L - a[i]) - b;
+		int s = std::upper_bound(b + 1, b + n + 1, L - a[i]) - b;
 		int e = std::lower_bound(b + 1, b + n + 1, R - a[i]) - b;
 		for (int j = s; j < e; j++) c[++m] = a[i] + b[j];
 	}
 	std::sort(c + 1, c + m + 1);
+	for (int i = count(L); i >= l; i--) printf("%lld ", L);
 	for (int i = 1; i <= m; i++) printf("%lld ", c[i]);
-	for (int i = m + 1; i <= r - l + 1; i++) printf("%lld ", R);
-    puts("");
+	for (int i = count(R - 1) + 1; i <= r; i++) printf("%lld ", R);
+	puts("");
 	return 0;
 }
