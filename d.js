70e36adf60ddfa7b5b226b69527ca1ca2d55c18a
ZhangZisu <admin@zhangzisu.cn>
2018-10-28 15:30:19 +0800

    update

--- d.js
+++
@@ -1,5 +0,0 @@
-const cfonts = require("cfonts")
-
-cfonts.say("GCC", { colors: ["yellow", "red"] });
-cfonts.say("AK", { colors: ["red", "green"] });
-cfonts.say("NOIP", { colors: ["green", "yellow"] });
--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 'use strict'
 
-const fs = require('fs');
+const fs = require('fs-extra')
 const path = require('path')
 const cfonts = require("cfonts")
 
-let ignore = [
-	'.git',
-	'.vscode',
-	'node_modules'
+const whitelist = [
+	/^\.[A-Za-z0-9]*$/,
+	/^node_modules$/,
 ];
 
-let extToDel = [
+const extToDel = [
 	'',
 	'.exe',
 	'.in',
@@ -21,29 +20,39 @@ let extToDel = [
 	'.table'
 ];
 
+const size_limit = 10240;
+
 function listFile(filePath) {
 	fs.readdir(filePath, function (err, files) {
 		if (err) {
 			console.warn(err);
 		} else {
 			files.forEach(function (filename) {
+				for (let whiteitem of whitelist) {
+					if (whiteitem.test(filename)) return;
+				}
 				let file = path.join(filePath, filename);
 				file = path.resolve(file);
 				fs.stat(file, function (eror, stats) {
 					if (eror) {
 						console.warn('Access denied.');
 					} else {
-						let isFile = stats.isFile();
-						let isDir = stats.isDirectory();
+						const isFile = stats.isFile();
+						const isDir = stats.isDirectory();
+						const size = stats.size;
 						if (isFile) {
 							if (extToDel.includes(path.extname(file))) {
 								console.log(`${file} has been deleted.`);
 								fs.unlinkSync(file);
 							}
+							if (size > size_limit) {
+								console.log(`${file} will be moved to workspace/tmp because of its size`);
+								fs.ensureDirSync(path.join(__dirname, "tmp/"));
+								fs.moveSync(file, path.join(__dirname, "tmp", filename));
+							}
 						}
 						if (isDir) {
-							if (!ignore.includes(filename))
-								listFile(file);
+							listFile(file);
 						}
 					}
 				})
