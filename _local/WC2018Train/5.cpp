77f339a2ad61c8c6c2101bf978dfa6476ba6a96d
ZhangZisu <admin@zhangzisu.cn>
2018-12-03 22:05:29 +0800

    Update

--- _local/WC2018Train/5.cpp
+++ _local/WC2018Train/5.cpp
@@ -12,7 +12,7 @@ inline void init() {
 	for (int i = 0; i < MAXV; i++) {
 		b_pow[i][0] = i;
 		for (int j = 1; j < 31; j++) {
-			b_pow[i][j] = pow[i][j - 1] * i;
+			b_pow[i][j] = b_pow[i][j - 1] * i;
 		}
 	}
 	for (int i = 0; i < (1 << 16); i++) {
@@ -73,14 +73,36 @@ struct bitset {
 				last = b_nxt[chunk];
 			}
 			chunk = (val[i] >> 16) & mask;
-			//
+			if (chunk == mask) {
+				last += 16;
+			} else {
+				last += b_pre[chunk];
+				ret += b_pow[last][k];
+				ret += b_val[chunk][k];
+				last = b_nxt[chunk];
+			}
 			chunk = (val[i] >> 32) & mask;
-			//
+			if (chunk == mask) {
+				last += 16;
+			} else {
+				last += b_pre[chunk];
+				ret += b_pow[last][k];
+				ret += b_val[chunk][k];
+				last = b_nxt[chunk];
+			}
 			chunk = (val[i] >> 48) & mask;
+			if (chunk == mask) {
+				last += 16;
+			} else {
+				last += b_pre[chunk];
+				ret += b_pow[last][k];
+				ret += b_val[chunk][k];
+				last = b_nxt[chunk];
+			}
 		}
 		return ret;
 	}
-	inline void bitset() {
+	inline bitset() {
 		clear();
 	}
 } data[SQRT][SQRT], tmp;
@@ -121,15 +143,16 @@ inline int lca(int u, int v) {
 		if (deep[fa[u][i]] >= deep[v]) u = fa[u][i];
 	for (int i = 17; ~i; i--)
 		if (fa[u][i] != fa[v][i]) u = fa[u][i], v = fa[v][i];
-	return u == v ? u : fa[u];
+	return u == v ? u : fa[u][0];
 }
+unsigned lans = 0;
 int main() {
 	init();
 	memset(head, -1, sizeof(head));
 	scanf("%d%d", &n, &m);
 	for (int i = 1; i <= n; i++) scanf("%d", &a[i]);
 	deep[1] = 1, pre(1, 0);
-	for (unsigned lans = 0, x; m; m--) {
+	for (int x, k, l; m; m--) {
 		scanf("%d", &x);
 		tmp.clear();
 		for (int u, v; x; x--) {
@@ -137,22 +160,22 @@ int main() {
 			u ^= lans;
 			v ^= lans;
 			int l = lca(u, v);
-			for (; !bp[u] && u != fa[lca]; u = fa[u][0]) tmp.set(a[u]);
-			for (; !bp[v] && v != fa[lca]; v = fa[v][0]) tmp.set(a[v]);
-			if (bp[u] && deep[pa[u]] >= deep[lca]) {
+			for (; !bp[u] && u != fa[l][0]; u = fa[u][0]) tmp.set(a[u]);
+			for (; !bp[v] && v != fa[l][0]; v = fa[v][0]) tmp.set(a[v]);
+			if (bp[u] && deep[pa[u]] >= deep[l]) {
 				int d = u;
-				while (pa[u] && deep[pa[pa[u]]] >= deep[lca]) u = pa[u];
+				while (pa[u] && deep[pa[pa[u]]] >= deep[l]) u = pa[u];
 				tmp.Or(data[d][u]);
 				u = pa[u];
 			}
-			if (bp[v] && deep[pa[v]] >= deep[lca]) {
+			if (bp[v] && deep[pa[v]] >= deep[l]) {
 				int d = v;
-				while (pa[v] && deep[pa[pa[v]]] >= deep[lca]) v = pa[v];
+				while (pa[v] && deep[pa[pa[v]]] >= deep[l]) v = pa[v];
 				tmp.Or(data[d][v]);
 				v = pa[v];
 			}
-			for (; u != fa[lca]; u = fa[u][0]) tmp.set(a[u]);
-			for (; v != fa[lca]; v = fa[v][0]) tmp.set(a[v]);
+			for (; u != fa[l][0]; u = fa[u][0]) tmp.set(a[u]);
+			for (; v != fa[l][0]; v = fa[v][0]) tmp.set(a[v]);
 		}
 		scanf("%d", &k);
 		printf("%u\n", lans = tmp.calc(k));
