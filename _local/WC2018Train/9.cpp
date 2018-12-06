e8a1856ec1c591725dda5433b5fe4f011992ed7c
ZhangZisu <admin@zhangzisu.cn>
2018-12-06 20:46:06 +0800

    update

--- _local/WC2018Train/9.cpp
+++ _local/WC2018Train/9.cpp
@@ -5,71 +5,56 @@
 #define MAXN 100010
 typedef long long lnt;
 const int INF = 0x3F3F3F3F;
-int n, m, a[MAXN], v[MAXN], id[MAXN], ans;
-int max[MAXN << 2], tag[MAXN << 2], L[MAXN << 2], R[MAXN << 2], cnt;
-inline void ensure(int &n) {
-	if (!n) {
-		n = ++cnt;
-		max[n] = -INF;
-		L[n] = R[n] = tag[n] = 0;
-	}
-}
+int n, m, a[MAXN], v[MAXN], ans;
+int max[MAXN << 2], tag[MAXN << 2];
 inline void pushDown(int n) {
 	if (tag[n]) {
-		ensure(L[n]);
-		ensure(R[n]);
-		max[L[n]] += tag[n];
-		tag[L[n]] += tag[n];
-		max[R[n]] += tag[n];
-		tag[R[n]] += tag[n];
+		max[n << 1] += tag[n];
+		tag[n << 1] += tag[n];
+		max[n << 1 | 1] += tag[n];
+		tag[n << 1 | 1] += tag[n];
 		tag[n] = 0;
 	}
 }
-inline void pushUp(int n) {
-	max[n] = -INF;
-	if (L[n] && max[L[n]] > max[n]) max[n] = max[L[n]];
-	if (R[n] && max[R[n]] > max[n]) max[n] = max[R[n]];
-}
-void set(int &n, int l, int r, int p, int v) {
-	ensure(n);
-	if (l == r) return max[n] = v, void();
+inline void add(int n, int l, int r, int L, int R, int v) {
+	if (L > R) return;
+	if (l == L && r == R) return max[n] += v, tag[n] += v, void();
 	pushDown(n);
 	int mid = (l + r) >> 1;
-	p <= mid ? set(L[n], l, mid, p, v) : set(R[n], mid + 1, r, p, v);
-	pushUp(n);
+	if (R <= mid) {
+		add(n << 1, l, mid, L, mid, v);
+	} else if (L > mid) {
+		add(n << 1 | 1, mid + 1, r, mid + 1, R, v);
+	} else {
+		add(n << 1, l, mid, L, mid, v);
+		add(n << 1 | 1, mid + 1, r, mid + 1, R, v);
+	}
+	max[n] = std::max(max[n << 1], max[n << 1 | 1]);
 }
-void add(int &n, int l, int r, int _l, int _r, int v) {
-	if (_r < _l) return;
-	ensure(n);
-	if (l == _l && r == _r) return max[n] += v, tag[n] += v, void();
-	int mid = (l + r) >> 1;
-	pushDown(n);
-	if (_r <= mid)
-		add(L[n], l, mid, _l, _r, v);
-	else if (_l > mid)
-		add(R[n], mid + 1, r, _l, _r, v);
-	else
-		add(L[n], l, mid, _l, mid, v), add(R[n], mid + 1, r, mid + 1, _r, v);
-	pushUp(n);
+lnt all;
+int upper[MAXN], lower[MAXN], mask[MAXN], used[MAXN], u, l;
+inline void add(int x, int v) {
+	int left = std::upper_bound(upper + 1, upper + u + 1, x, [](int x, int y) { return a[x] < a[y]; }) - upper;
+	int right = std::upper_bound(upper + 1, upper + u + 1, x) - upper - 1;
+	add(1, 1, u, left, right, v);
 }
-int bit[MAXN], tim[MAXN], bitclk;
-inline void clear() {
-	++bitclk;
-}
+namespace BIT {
+int bit[MAXN];
 inline int lowbit(int x) { return x & -x; }
 inline void add(int x, int y) {
-	for (; x <= n; x += lowbit(x)) bit[x] = tim[x] == bitclk ? bit[x] + y : y;
+	for (; x <= m; x += lowbit(x)) bit[x] += y;
 }
 inline int qry(int x) {
 	int y = 0;
-	for (; x; x -= lowbit(x)) y += tim[x] == bitclk ? bit[x] : 0;
+	for (; x; x -= lowbit(x)) y += bit[x];
 	return y;
 }
-lnt all;
-int upper[MAXN], lower[MAXN], u, l;
+}  // namespace BIT
+int id[MAXN];
 int main() {
 	scanf("%d", &n);
-	for (int i = 1; i <= n; i++) scanf("%d", a + i);
+	for (int i = 1; i <= n; i++) scanf("%d", &a[id[i] = i]);
+	std::sort(id + 1, id + n + 1, [](int x, int y) { return a[x] < a[y]; });
 	bool flag = 1;
 	for (int i = 1; i <= n; i++) flag &= ((a[i] - a[1]) == i - 1);
 	if (flag) return puts("1"), 0;
@@ -77,12 +62,20 @@ int main() {
 	std::sort(v + 1, v + n + 1);
 	m = std::unique(v + 1, v + n + 1) - v - 1;
 	for (int i = 1; i <= n; i++) a[i] = std::lower_bound(v + 1, v + m + 1, a[i]) - v;
-	for (int i = n; i >= 1; i--) all += qry(a[i]), add(a[i], 1);
+	for (int i = n; i >= 1; i--) all += BIT::qry(a[i]), BIT::add(a[i], 1);
 	for (int i = 1; i <= n; i++) {
-		if (a[i] > upper[u]) upper[++u] = i;
+		if (a[i] > upper[u]) mask[upper[++u] = i] = 1;
 	}
 	for (int i = n; i >= 1; i--) {
-		if (a[i] < lower[l]) lower[++l] = i;
+		if (a[i] < lower[l]) mask[lower[++l] = i] = 1;
+	}
+	std::reverse(lower + 1, lower + l + 1);
+	for (int i = 1, j = 1, k = 1; i <= l; i++) {
+		for (; j <= n && j < lower[i]; j++)
+			if (!mask[j]) add(j, 1), used[j] = 1;
+		ans = std::max(ans, max[1]);
+		for (; k < n && a[id[k]] <= a[lower[i + 1]]; k++)
+			if (used[k]) add(k, -1);
 	}
 	printf("%lld\n", all - ans);
 	return 0;
