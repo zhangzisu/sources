c472d1be69df4470093f7c1399e2fb95ec6b6b4a
ZhangZisu <admin@zhangzisu.cn>
2018-12-12 08:16:52 +0800

    update

--- LOJ/2840.cpp
+++ LOJ/2840.cpp
@@ -5,36 +5,36 @@
 #include <queue>
 #define MAXN 200010
 typedef long long lnt;
-int n, a[MAXN], pre[MAXN], nxt[MAXN];
-std::priority_queue<std::pair<int, int>> Q, P;
-lnt ans = 0;
-inline std::pair<int, int> top() {
-	while (P.size() && Q.top() == P.top()) Q.pop(), P.pop();
-	return Q.top();
+const lnt INF = (~0ULL) >> 2;
+int n, pre[MAXN], nxt[MAXN];
+lnt a[MAXN], ans = 0;
+std::priority_queue<std::pair<lnt, int>> Q, P;
+inline void remove(int x) {
+	if (~nxt[x]) pre[nxt[x]] = pre[x];
+	if (~pre[x]) nxt[pre[x]] = nxt[x];
 }
 int main() {
 	scanf("%d", &n);
 	for (int i = 1; i <= n; i++) {
-		scanf("%d", a + i);
+		scanf("%lld", a + i);
 		pre[i] = i - 1;
 		nxt[i] = i + 1;
 		Q.emplace(a[i], i);
 	}
+	pre[0] = -1, nxt[0] = 1, pre[n + 1] = n, nxt[n + 1] = -1;
+	Q.emplace(a[0] = -INF, 0);
+	Q.emplace(a[n + 1] = -INF, n + 1);
 	for (int t = (n + 1) >> 1; t; t--) {
-		auto x = top();
+		while (P.size() && Q.top() == P.top()) Q.pop(), P.pop();
+		auto x = Q.top().second;
 		Q.pop();
-		ans += x.first;
-		if (pre[x.second] >= 1) {
-			P.emplace(a[pre[x.second]], pre[x.second]);
-			Q.emplace(a[pre[x.second]] -= x.first, pre[x.second]);
-		}
-		if (nxt[x.second] <= n) {
-			P.emplace(a[nxt[x.second]], nxt[x.second]);
-			Q.emplace(a[nxt[x.second]] -= x.first, nxt[x.second]);
-		}
-		pre[nxt[x.second]] = pre[x.second];
-		nxt[pre[x.second]] = nxt[x.second];
-        printf("%lld\n", ans);
+		ans += a[x];
+		P.emplace(a[pre[x]], pre[x]);
+		P.emplace(a[nxt[x]], nxt[x]);
+		Q.emplace(a[x] = a[pre[x]] + a[nxt[x]] - a[x], x);
+		remove(pre[x]);
+		remove(nxt[x]);
+		printf("%lld\n", ans);
 	}
 	return 0;
 }
--- tmp/dp.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:loop
-d > in
-s <in > ans
-a <in > out
-fc ans out
-if not errorlevel 1 goto loop
