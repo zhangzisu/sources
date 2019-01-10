d630b9a0dd3812a13fa4f0b72ae70fa8a78ccc76
ZhangZisu <admin@zhangzisu.cn>
2019-01-10 16:33:55 +0800

    Update

--- clear.js
+++ clear.js
@@ -7,66 +7,51 @@ const cfonts = require("cfonts")
 const whitelist = [
 	/^\..*$/,
 	/^node_modules$/,
-	/^bigfiles$/
-];
-
-const extToKeep = [
-	/^\.c(c|pp|xx|s)?$/i,
-	/^\.j(ava|s|son)$/i,
-	/^\.h[a-z]*$/i,
-	/^\.pyo?$/i,
-	/^\.sh$/i,
-	/^\.bat$/i,
-	/^\.m(d|arkdown)$/i,
-	/^\.[a-z]+ml$/i,
-	/^\.ps[0-9]?$/i
-];
-
-const size_limit = 50 * 1024;     // 50KB
+	/^bigfiles$/,
+	/^yarn.lock$/,
+	/\.c(c|pp|xx|s)?$/i,
+	/\.j(ava|s|son)$/i,
+	/\.h[a-z]*$/i,
+	/\.pyo?$/i,
+	/\.sh$/i,
+	/\.bat$/i,
+	/\.m(d|arkdown)$/i,
+	/\.[a-z]+ml$/i,
+	/\.ps[0-9]?$/i
+]
 
 function match(list, string) {
 	for (let item of list) {
-		if (item.test(string)) return true;
+		if (item.test(string)) return true
 	}
-	return false;
+	return false
 }
 
 function listFile(filePath) {
 	fs.readdir(filePath, function (err, files) {
 		if (err) {
-			console.warn(err);
+			console.warn(err)
 		} else {
 			files.forEach(function (filename) {
-				if (match(whitelist, filename)) return;
-				let file = path.join(filePath, filename);
-				file = path.resolve(file);
-				fs.stat(file, function (eror, stats) {
-					if (eror) {
-						console.warn('Access denied.');
-					} else {
-						const isFile = stats.isFile();
-						const isDir = stats.isDirectory();
-						const size = stats.size;
-						if (isFile) {
-							if (!match(extToKeep, path.extname(file))) {
-								console.log(`${file} has been deleted.`);
-								fs.unlinkSync(file);
-							} else if (size > size_limit) {
-								console.log(`${file} will be moved to workspace/bigfiles because of its size`);
-								fs.ensureDirSync(path.join(__dirname, "bigfiles"));
-								fs.moveSync(file, path.join(__dirname, "bigfiles", filename));
-							}
-						}
-						if (isDir) {
-							listFile(file);
-						}
+				if (match(whitelist, filename)) return
+				let file = path.join(filePath, filename)
+				file = path.resolve(file)
+				const stats = fs.statSync(file)
+				const isDir = stats.isDirectory()
+				if (isDir) {
+					listFile(file)
+					if (!fs.readdirSync(file).length) {
+						fs.removeSync(file)
 					}
-				})
-			});
+				} else {
+					console.log(`${file} has been deleted.`)
+					fs.unlinkSync(file)
+				}
+			})
 		}
-	});
+	})
 }
 
-cfonts.say("source-|OJ", { colors: ["yellow"] });
+cfonts.say("source-|contest", { colors: ["yellow"] })
 
-listFile('.');
+listFile('.')
